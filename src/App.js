81815f4acdd1e4a01f1317bd10ff6d40b92d65f0
SAAD-ELGHABA <saadlraba@gmail.com>
2024-11-14 22:22:16 +0100

    file Meteo.js has added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Meteo from './components/Meteo.js';
 
 function App() {
   return (
     <div>
-      
+      <Meteo/>
     </div>
   );
 }
